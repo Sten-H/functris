bd80e560cfd3a6dd060daee97671b283dc29bfbb
Sten-H <mail@stenh.com>
2017-10-29 01:22:19 +0200

    Now validates before rotation

--- src/reducers/tetris/logic/logic.js
+++ src/reducers/tetris/logic/logic.js
@@ -1,8 +1,9 @@
 import {
     add, any, compose, converge, curry, dec, equals, flip, head, identity, ifElse, inc, last, lensIndex, map, multiply,
-    over, path, zipWith, reverse, __, always, cond, and, when, gte, lt, view, or, not, lensProp
+    over, path, zipWith, reverse, __, always, cond, and, when, gte, lt, view, or, not, lensProp, allPass, anyPass
 } from "ramda";
 import * as constants from './constants';
+import { ROW_COUNT } from "./constants/index";
 // LENSES
 // State lenses
 export const posLens = lensProp('pos');
@@ -19,63 +20,83 @@ export const addCoords = zipWith(add);
 export const getCell = curry((board, coord) => compose(
     flip(path)(board),
     reverse)(coord));
-// POSITION FUNCTIONS
 
-const shift = curry((dirFunc, state) => over(posLens, dirFunc, state));
+// POSITION FUNCTIONS
+// directions are used as transformers for shift function
 const leftDir = over(xLens, dec);
 const rightDir = over(xLens, inc);
-/**
- * Shifts position
- */
-// coord -> boolean
-export const isCoordOutOfBounds = compose(
-    converge(or, [lt(__, 0), gte(__, constants.COL_COUNT)]),
-    view(xLens)
-);
-// (pos -> piece) -> boolean
-export const isPieceOutOfBounds = curry(({pos, piece}) => compose(
-    any(isCoordOutOfBounds),
-    pieceActualPosition(pos)
-)(piece));
-//
+// f -> state -> state, takes transformer (leftDir/rightDir), and state as arguments
+const shift = over(posLens);
 
-const shiftHorizontal = curry((shiftFunc, state) =>
-    ifElse(
-        compose(
-            not,
-            converge(or,
-                [
-                    isPieceOutOfBounds,
-                    isPieceOverlapping
-                ]),
-            shiftFunc
+// (lens, [predicates] -> f(coord) -> boolean
+const coordValidator = (lens, predicates) =>
+    compose(
+        anyPass(predicates),
+        view(lens)
+    );
+const isXOutOfBounds = coordValidator(
+    xLens,
+    [
+        lt(__, 0),
+        gte(__, constants.COL_COUNT)
+    ]);
+const isYOutOfBounds = coordValidator(
+    yLens,
+    [
+        gte(__, 20)
+    ]);
+// coord -> boolean
+export const isCoordOutOfBounds = anyPass([isXOutOfBounds, isYOutOfBounds]);
+// (pos -> piece) -> boolean
+export const isPieceOutOfBounds = curry((state) => compose(
+    any(isCoordOutOfBounds),
+    pieceActualPosition)(state));
+// f -> state -> boolean, validates transformed state, returns true if valid
+const transformAndValidate = transformFunc =>
+    compose(
+        not,
+        converge(
+            or,
+            [
+                isPieceOutOfBounds,
+                isPieceOverlapping
+            ]
         ),
-        shiftFunc,
-        always(state),
+        transformFunc
+    );
+/**
+ * tries to transform state with transformer func, returns updated state if new state
+ * is valid, otherwise returns old state.
+ */
+const tryTransform = curry((transformFunc, state) =>
+    when(
+        transformAndValidate(transformFunc),
+        transformFunc,
     )(state));
-export const shiftLeft = shiftHorizontal(shift(leftDir));
-export const shiftRight = shiftHorizontal(shift(rightDir));
+
+export const shiftLeft = tryTransform(shift(leftDir));
+export const shiftRight = tryTransform(shift(rightDir));
 
 // PIECE FUNCTIONS
 // n -> n, transforms -0 to 0
-const normalizeZero = cond([
-    [equals(-0), Math.abs],
-    [always(true), identity]
-]);
+const normalizeZero = when(equals(-0), Math.abs);
+// c -> c Sets all -0 to 0 in coord.
+const normalizeCoord = map(normalizeZero);
+
 // adds position value to each piece coord to get true position
-export const pieceActualPosition = curry((pos, piece) => map(addCoords(pos), piece));
-// board -> coord -> boolean
-export const isCoordOverlapping = board => compose(
+export const pieceActualPosition = curry(({pos, piece}) => map(addCoords(pos), piece));
+
+// state -> coord -> boolean
+export const isCoordOverlapping = ({board}) => compose(
     equals("X"),
     getCell(board));
-export const isPieceOverlapping = curry(({pos, piece, board}) => {
-    const actualPiece = pieceActualPosition(pos, piece);
-    return any(isCoordOverlapping(board), actualPiece)
-});
-/**
- * Sets all -0 to 0 to make testing easier.
- */
-const normalizeCoord = map(normalizeZero);
+
+// state -> boolean
+export const isPieceOverlapping = state =>
+    compose(
+        any(isCoordOverlapping(state)),
+        pieceActualPosition)(state);
+
 const clockwise = [compose(multiply(-1), last), head];
 const counterClockwise = [last, compose(multiply(-1), head)];
 /**
@@ -88,5 +109,6 @@ const rotateCoord = converge(
         Array.of
     )
 );
-export const rotatePieceClockwise = map(rotateCoord(clockwise));
-export const rotatePieceCounter = map(rotateCoord(counterClockwise));
+const rotatePiece = (dirFunc) => over(pieceLens, map(rotateCoord(dirFunc)));
+export const rotateClockwise = tryTransform(rotatePiece(clockwise));
+export const rotateCounterClockwise = tryTransform(rotatePiece(counterClockwise));
--- src/reducers/tetris/logic/logic.test.js
+++ src/reducers/tetris/logic/logic.test.js
@@ -1,13 +1,18 @@
 import {
-    shiftLeft, shiftRight, rotatePieceClockwise, rotatePieceCounter, isPieceOverlapping,
-    pieceActualPosition, isCoordOverlapping, getCell, isCoordOutOfBounds, isPieceOutOfBounds, posLens, boardLens
+    shiftLeft, shiftRight, rotateClockwise, rotateCounterClockwise, isPieceOverlapping,
+    pieceActualPosition, isCoordOverlapping, getCell, isCoordOutOfBounds, isPieceOutOfBounds, posLens, boardLens,
+    pieceLens
 } from "./logic";
-import { adjust, all, compose, dec, equals, inc, lensProp, map, over, path, repeat, set, update } from "ramda";
+import {
+    adjust, all, compose, dec, equals, inc, lensProp, map, over, path, prop, repeat, set, update,
+    view
+} from "ramda";
 import { ROW_COUNT, EMPTY_BOARD, EMPTY_TOKEN, FILL_TOKEN, COL_COUNT } from "./constants/index";
 
 describe('Tetris logic', () => {
     const emptyBoard = EMPTY_BOARD;
     const emptyRow = emptyBoard[0];
+    const filledRow = repeat(FILL_TOKEN, ROW_COUNT);
     const LPiece = [ [ 1, -1], [-1,  0], [ 0,  0], [ 1,  0] ];  // L piece: -->  ___|
     const IPiece = [ [-1,  0], [0,  0], [ 1,  0], [ 2,  0] ];  // I piece --> ____
     const pos = [0 ,0];
@@ -23,10 +28,14 @@ describe('Tetris logic', () => {
             expect(isCoordOutOfBounds([9, 19])).toBe(false);
             expect(isCoordOutOfBounds([10, 7])).toBe(true);
         });
-        it('should detect piece out of bounds', () => {
+        it('should detect piece out of x bounds', () => {
             expect(isPieceOutOfBounds(state)).toBe(true);
             const s = set(posLens, [1, 0], state);
             expect(isPieceOutOfBounds(s)).toBe(false);
+        });
+        it('should detect piece out of lower y bounds', () => {
+            const s = set(posLens, [5, 20], state);
+            expect(isPieceOutOfBounds(s)).toBe(true);
         });
     });
     describe('Shift horizontal', () => {
@@ -36,9 +45,9 @@ describe('Tetris logic', () => {
             expect(shiftLeft(s).pos).toEqual(expected);
         });
         it('Should return call value if piece out of bounds after shift', () => {
-            // const s1 = set(posLens, [0, 5], state);
-            // const expected1 = [0, 5];
-            // expect(shiftLeft(s1).pos).toEqual(expected1);
+            const s1 = set(posLens, [0, 5], state);
+            const expected1 = [0, 5];
+            expect(shiftLeft(s1).pos).toEqual(expected1);
             const s2 = set(posLens, [7, 10], state);
             const expected2 = [7, 10];
             // Check to see that it is valid before move
@@ -51,25 +60,71 @@ describe('Tetris logic', () => {
         })
     });
     describe('Rotate', () => {
+        const s1 = compose(
+            set(posLens, [5, 5]),
+            set(pieceLens, LPiece)
+        )(state);
         it('Should rotate clockwise', () => {
-            const LRotated = rotatePieceClockwise(LPiece);
-            const LExpected = [ [ 1,  1], [ 0, -1], [ 0,  0], [ 0,  1] ];
-            expect(LRotated).toEqual(LExpected);
+            const expected = [ [ 1,  1], [ 0, -1], [ 0,  0], [ 0,  1] ];
+            expect(rotateClockwise(s1).piece).toEqual(expected);
         });
         it('Should rotate counter clockwise', () => {
-            const LRotated = rotatePieceCounter(LPiece);
-            const LExpected = [ [-1, -1], [ 0,  1], [ 0,  0], [ 0, -1] ];
-            expect(LRotated).toEqual(LExpected);
+            const expected = [ [-1, -1], [ 0,  1], [ 0,  0], [ 0, -1] ];
+            expect(rotateCounterClockwise(s1).piece).toEqual(expected);
         });
-        it('Should return call value if piece out of bounds after rotation', () => {
-            expect(true).toBe(false);
+        it('Rotate counter then clockwise should revert to original', () => {
+            const expected = prop('piece')(s1);
+            const rotated = compose(
+                prop('piece'),
+                rotateClockwise,
+                rotateCounterClockwise
+            )(s1);
+            expect(rotated).toEqual(expected);
+        });
+        it('piece rotated counter should not be equal piece rotated clockwise (except OPiece)', () => {
+            const s1 = compose(
+                set(posLens, [5, 5]),
+                set(pieceLens, LPiece)
+            )(state);
+            expect(rotateClockwise(s1).piece).not.toEqual(rotateCounterClockwise(s1).piece)
+            const s2 = compose(
+                set(posLens, [5, 5]),
+                set(pieceLens, IPiece)
+            )(state);
+            expect(rotateClockwise(s2).piece).not.toEqual(rotateCounterClockwise(s2).piece)
+        });
+        it('Should return call value if piece out of x bounds after rotation', () => {
+            const s = compose(
+                rotateClockwise,
+                set(posLens, [0, 5]),
+            )(state);  // Stick piece is upright and on left border, will be out of bounds if rotated
+            const expected = view(pieceLens, s);
+            expect(rotateClockwise(s).piece).toEqual(expected);
+        });
+        it('Should return call value if piece out of lower y bound after rotation', () => {
+            // FIXME this should probably later raise the piece instead of invalidating
+            const s = compose(
+                set(posLens, [0, 19]),
+                set(pieceLens, IPiece), // laying down stick piece
+            )(state);  // rotating will make stick go though floor
+            const expected = view(pieceLens, s);
+            expect(rotateCounterClockwise(s).piece).toEqual(expected);
         });
         it('Should return call value if piece overlapping after rotation', () => {
-            expect(true).toBe(false);
+            const filledRowBoard = update(dec(ROW_COUNT), filledRow, emptyBoard);
+            const s = compose(
+                set(posLens, [0, 18]),
+                set(pieceLens, IPiece),
+                set(boardLens, filledRowBoard)
+            )(state);
+            // check to see that it doesn't overlap before rotation
+            expect(isPieceOverlapping(s)).toBe(false);
+            const expected = view(pieceLens, s);
+            expect(rotateClockwise(s).piece).toEqual(expected);
+            expect(rotateCounterClockwise(s).piece).toEqual(expected);
         });
     });
     describe('Board', () => {
-        const filledRow = repeat(FILL_TOKEN, COL_COUNT);
         describe('Set up', () => {
             it('should have 20 rows and 10 columns', () => {
                 expect(emptyBoard).toHaveLength(20);
@@ -87,16 +142,16 @@ describe('Tetris logic', () => {
     });
     describe('Overlap detection', () => {
         it('should detect overlap', () => {
-            const filledRow = repeat(FILL_TOKEN, ROW_COUNT);
             const board = update(dec(ROW_COUNT), filledRow, EMPTY_BOARD);  // Bottom row filled
-            const state1 = compose(
+            const s1 = compose(
                 set(posLens, [5, dec(ROW_COUNT)]),  // Bottom middle
                 set(boardLens, board)
             )(state);
             // IPiece is laying down should overlap
-            expect(isPieceOverlapping(state1)).toBe(true);
-            const state2 = over(posLens, adjust(inc, 1), state);
-            expect(isPieceOverlapping(state2)).toBe(false);
+            expect(isPieceOverlapping(s1)).toBe(true);
+            // Raise pos by 1, should no longer overlap
+            const s2 = over(posLens, adjust(inc, 1), state);
+            expect(isPieceOverlapping(s2)).toBe(false);
         });
     });
 });
